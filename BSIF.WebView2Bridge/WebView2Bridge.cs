e8f7290faf46b744c5cfddcd4821fb5def2e7167
Fina <42113148+BSPR0002@users.noreply.github.com>
2024-05-29 18:55:30 +0800

    最终方案

--- BSIF.WebView2Bridge/WebView2Bridge.cs
+++ BSIF.WebView2Bridge/WebView2Bridge.cs
@@ -1,15 +1,14 @@
 ﻿namespace BSIF.WebView2Bridge;
 
 using System.Runtime.InteropServices;
-using System.Collections.ObjectModel;
 
 [ClassInterface(ClassInterfaceType.AutoDual)]
 [ComVisible(true)]
-public class WebView2Bridge(WebView2Bridge.Caller pythonCaller, string[] methodsName)
+public class WebView2Bridge(WebView2Bridge.Caller pythonCaller, string[] methodNames)
 {
 	public delegate string Caller(string methodName, string argsJson);
 	private readonly Caller caller = pythonCaller;
-	public readonly ReadOnlyCollection<string>? methodsName = new(methodsName);
+	public readonly string[] methodNames = methodNames;
 	public string Call(string methodName, string argsJson)
 	{
 		return caller.Invoke(methodName, argsJson);
