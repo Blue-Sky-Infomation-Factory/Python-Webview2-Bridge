878864c0f23a609e8283ccb25739f3d34cca8de6
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2024-06-03 02:53:02 +0800

    async call

--- BSIF.WebView2Bridge/WebView2Bridge.cs
+++ BSIF.WebView2Bridge/WebView2Bridge.cs
@@ -4,13 +4,20 @@ using System.Runtime.InteropServices;
 
 [ClassInterface(ClassInterfaceType.AutoDual)]
 [ComVisible(true)]
-public class WebView2Bridge(WebView2Bridge.SyncCaller pythonSyncCaller, string[] methodNames)
+public class WebView2Bridge(WebView2Bridge.SyncCaller syncCaller, WebView2Bridge.AsyncCaller asyncCaller, string[] methodNames)
 {
-	public delegate string SyncCaller(string methodName, string argsJson);
-	private readonly SyncCaller syncCaller = pythonSyncCaller;
 	public readonly string[] methodNames = methodNames;
+	public delegate string SyncCaller(string methodName, string argsJson);
+	private readonly SyncCaller syncCaller = syncCaller;
 	public string SyncCall(string methodName, string argsJson)
 	{
 		return syncCaller.Invoke(methodName, argsJson);
+	}
+	public delegate void AsyncCaller(string methodName, string argsJson, TaskCompletionSource<string> asyncObject);
+	private readonly AsyncCaller asyncCaller = asyncCaller;
+	public Task<string> AsyncCall(string methodName, string argsJson){
+		TaskCompletionSource<string> taskSource = new();
+		asyncCaller.Invoke(methodName, argsJson, taskSource);
+		return taskSource.Task;
 	}
 }
